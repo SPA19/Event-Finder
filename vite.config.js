0cde2af634f28ba9ca4f74aa9f3d34dfab34e6ec
Simón Posada Dev <simon.150@hotmail.com>
2025-12-03 11:41:56 -0500

    Actualizar la configuración de Vite para cambiar la base de la aplicación a una ruta relativa.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://SPA19.github.io/Event-Finder",
+	plugins: [react()],
+	base: "/Event-Finder",
 });
